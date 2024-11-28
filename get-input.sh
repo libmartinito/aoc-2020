16c79da26f2587045d7659504ed3818baccfced8
libmartinito <lib.martinito@pm.me>
2024-11-28 15:39:54 +0800

    fix: lint fix and added header to fetch request

--- get-input.sh
+++ get-input.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-if ["$#" -ne 2]; then
+if [ "$#" -ne 2 ]; then
   echo "Usage: $0 <year> <day>"
   exit 1
 fi
 
-YEAR = "$1"
-DAY = "$2"
+YEAR="$1"
+DAY="$2"
 
-URL = "https://adventofcode.com/$YEAR/day/$DAY/input"
-OUTPUT_FILE = "input/day-$DAY.txt"
+URL="https://adventofcode.com/$YEAR/day/$DAY/input"
+HEADER="Cookie: session=$AOC_TOKEN"
+OUTPUT_FILE="input/day-$DAY.txt"
 
-curl -s -L "$URL" -o "$OUTPUT_FILE"
+curl -s -L -H "$HEADER" "$URL" -o "$OUTPUT_FILE"
 
 echo "Raw body written to $OUTPUT_FILE"
